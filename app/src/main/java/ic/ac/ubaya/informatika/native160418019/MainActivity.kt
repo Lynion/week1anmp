bf3d4f2675a421b8e953ad45f98f4d021b716dc4
Dylan Sherwood <dylansherwood35@gmail.com>
2021-08-12 13:57:24 +0700

    initial commit

---
+++ app/src/main/java/ic/ac/ubaya/informatika/native160418019/MainActivity.kt
@@ -0,0 +1,11 @@
+package ic.ac.ubaya.informatika.native160418019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
